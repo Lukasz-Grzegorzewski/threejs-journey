d75b33ee83eb5452ade81bf796c9b5e4a919f64d
lukasz <grzegorzewski.luk@gmail.com>
2026-03-07 10:31:27 +0100

    add: reaging sea effect based on elapsedTime. Elevation, Frequency plus all those to small waves(iterations to choose)

---
+++ 4.29_Raging_Sea/src/shaders/water/fragment.glsl
@@ -0,0 +1,16 @@
+// color
+uniform vec3 uDepthColor;
+uniform vec3 uSurfaceColor;
+uniform float uColorOffset;
+uniform float uColorMultiplier;
+
+varying float vElevation;
+
+void main()
+{
+  float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
+  vec3 finalColor = mix(uDepthColor, uSurfaceColor, mixStrength);
+
+  gl_FragColor = vec4(finalColor, 1.0);
+  #include <colorspace_fragment>
+}
