1bdcf089c9db72b3eafa01c946873663f4f196f3
lukasz <grzegorzewski.luk@gmail.com>
2026-03-22 21:24:55 +0100

    add animated transition between the models(gll) as particles based on the Progress. Simplex noise for the particles start time of transitions and end time.

--- 4.40_Particles_Morphing_Shader/src/shaders/particles/fragment.glsl
+++ 4.40_Particles_Morphing_Shader/src/shaders/particles/fragment.glsl
@@ -1,6 +1,14 @@
+varying vec3 vColor;
+
 void main()
 {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-    #include <tonemapping_fragment>
-    #include <colorspace_fragment>
+  vec2 uv = gl_PointCoord;
+
+  // alias = distance(uv, vec2(0.5));
+  float distanceToCenter = length(uv - 0.5);
+  float alpha = 0.05 / distanceToCenter - 0.1;
+
+  gl_FragColor = vec4(vColor, alpha);
+  #include <tonemapping_fragment>
+  #include <colorspace_fragment>
 }
--- 4.40_Particles_Morphing_Shader/src/shaders/particles/vertex.glsl
+++ 4.40_Particles_Morphing_Shader/src/shaders/particles/vertex.glsl
@@ -1,15 +1,42 @@
 uniform vec2 uResolution;
 uniform float uSize;
+uniform float uProgress;
+uniform vec3 uColorA;
+uniform vec3 uColorB;
+
+attribute vec3 aPositionTarget;
+attribute float aSize;
+
+varying vec3 vColor;
+
+#include ../includes/simplexNoise.glsl;
 
 void main()
 {
-    // Final position
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    vec4 viewPosition = viewMatrix * modelPosition;
-    vec4 projectedPosition = projectionMatrix * viewPosition;
-    gl_Position = projectedPosition;
+  // Mixed position
+  float noiseOrigin = simplexNoise3d(position * 0.2);
+  float noiseTarget = simplexNoise3d(aPositionTarget * 0.2);
+  float noise = mix(noiseOrigin, noiseTarget, uProgress);
+  noise = smoothstep(-1.0, 1.0, noise);
 
-    // Point size
-    gl_PointSize = uSize * uResolution.y;
-    gl_PointSize *= (1.0 / - viewPosition.z);
+  float duration = 0.4;
+  float maxDelay = 1.0 - duration;
+  float delay = maxDelay * noise;
+  float end = delay + duration;
+
+  float progress = smoothstep(delay, end, uProgress);
+  vec3 mixedPosition = mix(position,aPositionTarget, progress);
+
+  // Final position
+  vec4 modelPosition = modelMatrix * vec4(mixedPosition, 1.0);
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedPosition = projectionMatrix * viewPosition;
+  gl_Position = projectedPosition;
+
+  // Point size
+  gl_PointSize = uSize * uResolution.y * aSize;
+  gl_PointSize *= (1.0 / - viewPosition.z);
+
+  // Varyings
+  vColor = mix(uColorA, uColorB, noise);
 }
