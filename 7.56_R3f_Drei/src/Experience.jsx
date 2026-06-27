11c0e80b86b84fceaf752edae1e9351045efca49
lukasz <grzegorzewski.luk@gmail.com>
2026-06-27 15:26:52 +0200

    Add Drei helpers and interactive scene controls. Integrate OrbitControls, PivotControls, TransformControls, HTML labels with occlusion, floating 3D text, and a reflective floor using MeshReflectorMaterial.

--- 7.56_R3f_Drei/src/Experience.jsx
+++ 7.56_R3f_Drei/src/Experience.jsx
@@ -1,32 +1,78 @@
-import { useThree, extend } from "@react-three/fiber";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
-extend({ OrbitControls });
+import {
+  MeshReflectorMaterial,
+  Float,
+  Html,
+  Text,
+  PivotControls,
+  TransformControls,
+  OrbitControls,
+} from "@react-three/drei";
+import { useRef } from "react";
 
 export default function Experience() {
-  const { camera, gl } = useThree();
+  const cubeRef = useRef();
 
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
+      <OrbitControls makeDefault />
 
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={4}
+        axisColors={["#9381ff", "#ff4d6d", "#00b8d4"]}
+        scale={100} // size of the pivot
+        fixed={true} // perspective camera on/off. Off by default.
+      >
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+          <Html
+            wrapperClass="label"
+            position={[1, 1, 0]}
+            center
+            distanceFactor={8}
+            occlude={[cubeRef]}
+          >
+            This is a sphere 👋
+          </Html>
+        </mesh>
+      </PivotControls>
 
-      <mesh position-x={2} scale={1.5}>
+      <mesh ref={cubeRef} position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
+      <TransformControls object={cubeRef} mode="translate" />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
+        <MeshReflectorMaterial
+          resolution={512}
+          blur={[1000, 1000]}
+          mixBlur={1}
+          mirror={0.75}
+          color={"greenyellow"}
+        />
+        {/* <meshStandardMaterial color="greenyellow" /> */}
       </mesh>
+
+      <Float speed={5} floatIntensity={10}>
+        <Text
+          font="./bangers-v20-latin-regular.woff"
+          fontSize={1}
+          color={"salmon"}
+          position-y={2}
+          maxWidth={2}
+          textAlign="center"
+        >
+          I LOVE R3F
+          {/* <meshNormalMaterial /> */}
+        </Text>
+      </Float>
     </>
   );
 }
